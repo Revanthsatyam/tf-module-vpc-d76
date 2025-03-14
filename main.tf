4713382725c7cc61fcec07c91641e2f5f13c7dba
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-14 19:52:53 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr
   tags       = merge(local.tags, { Name = "${var.env}-vpc" })
 }
+
+subnets = var.subnets
+
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#
+#   for_each = var.subnets
+#   cidr_block = "10.0.1.0/24"
+#
+#   tags = merge(local.tags, { Name = "${var.env}-subnet" })
+# }
