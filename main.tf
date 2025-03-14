639af55f25cbd1c4ebb2cad57695ec1ced034336
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-14 20:03:16 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,14 +2,3 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr
   tags       = merge(local.tags, { Name = "${var.env}-vpc" })
 }
-
-resource "aws_subnet" "main" {
-  vpc_id = aws_vpc.main.id
-
-  for_each          = var.subnets
-  subnets           = each.value
-  #cidr_block        = "10.0.1.0/24"
-  #availability_zone = ""
-
-  tags = merge(local.tags, { Name = "${var.env}-subnet" })
-}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "subnet" {
-  value = aws_subnet.main.subnets
+  value = var.subnets
 }
