6b8baf64c3ca9976ccc59a148c3d6f8621ebda86
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-14 21:38:31 +0530

    changes

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,9 +1,15 @@
 resource "aws_subnet" "main" {
-  vpc_id = var.vpc_id
-
   for_each          = var.subnets
+  vpc_id            = var.vpc_id
   cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
 
   tags = merge(var.tags, { Name = "${var.env}-${each.key}-subnet" })
+}
+
+resource "aws_route_table" "example" {
+  for_each = var.subnets
+  vpc_id   = var.vpc_id
+
+  tags = merge(var.tags, { Name = "${var.env}-${each.key}-RT" })
 }
