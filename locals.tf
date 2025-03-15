5cb6573de2991fbf047134bc7a7347b3474492e4
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-15 12:40:29 +0530

    changes

--- locals.tf
+++ locals.tf
@@ -3,13 +3,13 @@ locals {
   public_subnets = [for k, v in lookup(lookup(module.subnets, "public", null), "subnet", null) : v.id]
   app_subnets    = [for k, v in lookup(lookup(module.subnets, "app", null), "subnet", null) : v.id]
   db_subnets     = [for k, v in lookup(lookup(module.subnets, "db", null), "subnet", null) : v.id]
-  private_subnets = tolist(merge(local.app_subnets, local.db_subnets))
+  private_subnets = concat(local.app_subnets, local.db_subnets)
 
   #Route_Tables
   public_route_tables  = [for k, v in lookup(lookup(module.subnets, "public", null), "route_table", null) : v.id]
   app_route_tables     = [for k, v in lookup(lookup(module.subnets, "app", null), "route_table", null) : v.id]
   db_route_tables      = [for k, v in lookup(lookup(module.subnets, "db", null), "route_table", null) : v.id]
-  #private_route_tables = merge(local.app_route_tables, local.db_route_tables)
+  private_route_tables = concat(local.app_route_tables, local.db_route_tables)
 
   tags = merge(var.tags, { tf-module-name = "vpc" }, { env = var.env })
 }
