7ff99508c5f78078f4e5a024108fdecbdffe35e3
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-15 12:36:22 +0530

    changes

--- outputs.tf
+++ outputs.tf
@@ -2,26 +2,26 @@ output "subnets" {
   value = module.subnets
 }
 
-output "public_subnets" {
-  value = local.public_subnets
-}
-
-output "app_subnets" {
-  value = local.app_subnets
-}
-
-output "db_subnets" {
-  value = local.db_subnets
-}
-
-output "public_route_tables" {
-  value = local.public_route_tables
-}
-
-output "app_route_tables" {
-  value = local.app_route_tables
-}
-
-output "db_route_tables" {
-  value = local.db_route_tables
-}
+# output "public_subnets" {
+#   value = local.public_subnets
+# }
+#
+# output "app_subnets" {
+#   value = local.app_subnets
+# }
+#
+# output "db_subnets" {
+#   value = local.db_subnets
+# }
+#
+# output "public_route_tables" {
+#   value = local.public_route_tables
+# }
+#
+# output "app_route_tables" {
+#   value = local.app_route_tables
+# }
+#
+# output "db_route_tables" {
+#   value = local.db_route_tables
+# }
