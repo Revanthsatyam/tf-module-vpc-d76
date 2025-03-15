e036bbf46d7694bdabc2f1d1f6d7c3fd41fdbe61
Revanthsatyam <satyamrevanth@gmail.com>
2025-03-15 12:33:23 +0530

    changes

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,23 @@ output "subnets" {
 output "public_subnets" {
   value = local.public_subnets
 }
+
+output "app_subnets" {
+  value = local.app_subnets
+}
+
+output "db_subnets" {
+  value = local.db_subnets
+}
+
+output "public_route_tables" {
+  value = local.public_route_tables
+}
+
+output "app_route_tables" {
+  value = local.app_route_tables
+}
+
+output "db_route_tables" {
+  value = local.db_route_tables
+}
